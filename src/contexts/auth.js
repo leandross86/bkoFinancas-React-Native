dd9cef66e4d4cff6716a16242ea975e0b035fe2e
leandross86 <leandrobko@gmail.com>
2020-09-27 12:18:38 -0300

    create user-login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,10 +1,51 @@
-import React, { useState, createContext } from 'react'
+import React, { 
+  useState, 
+  createContext,
+  useEffect
+} from 'react'
+import { Alert } from 'react-native'
 import firebase from '../services/firebaseConnetion'
+import AsyncStorage from '@react-native-community/async-storage'
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }){
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadStorage(){
+      const storageUser = await AsyncStorage.getItem('Auth_user')
+
+      if (storageUser){
+        setUser(JSON.parse(storageUser))
+        setLoading(false)
+      }
+      setLoading(false)
+    }
+    loadStorage()
+  }, [])
+
+  //Função para logar usuario
+  async function signIn(email, password){
+    await firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(async (value) => {
+      let uid = value.user.uid
+      await firebase.database().ref('users').child(uid).once('value')
+      .then((snapshot) => {
+        let data = {
+          uid: uid,
+          nome: snapshot.val().nome,
+          email: value.user.email
+        }
+        setUser(data)
+        storegeUser(data)
+      })
+    })
+    .catch((error) => {
+      Alert(error.code)
+    })
+  }
 
   // Cadastrar usuario
   async function signUp(email, password, nome){
@@ -22,12 +63,20 @@ function AuthProvider({ children }){
           email: value.user.email,
         }
         setUser(data)
+        storegeUser(data)
       })
+    })
+    .catch((error) => {
+      Alert(error.code)
     })
   }
 
+  async function storegeUser(data){
+    await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signUp }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loading }}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 
+import { AuthContext } from '../../contexts/auth'
+
 export default function Home() {
+  const { user } = useContext(AuthContext)
   return (
     <View>
       <Text>Home</Text>
+      <Text>{user && user.nome}</Text>
+      <Text>{user && user.email}</Text>
     </View>
   );
 }
