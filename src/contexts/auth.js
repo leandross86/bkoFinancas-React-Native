b1fe8ed89a39a5a5a9b7f430968b776a021601c4
leandross86 <leandrobko@gmail.com>
2020-09-27 13:13:51 -0300

    create logging-out-user

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -75,8 +75,24 @@ function AuthProvider({ children }){
     await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
   }
 
+  async function  signOut() {
+    await firebase.auth().signOut()
+    await AsyncStorage.clear()
+    .then(() => {
+      setUser(null)
+    })
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loading }}>
+    <AuthContext.Provider value={{ 
+      signed: 
+        !!user, 
+        user, 
+        loading,
+        signUp, 
+        signIn, 
+        signOut,  
+    }}>
       {children}
     </AuthContext.Provider>
   )
