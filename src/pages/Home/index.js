eb39c72e2bac9f46a4902d5c4c41d6195bb6ded2
leandross86 <leandrobko@gmail.com>
2020-10-01 20:55:07 -0300

    show records

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import firebase from '../../services/firebaseConnetion'
+import { format } from 'date-fns'
 import { AuthContext } from '../../contexts/auth'
 import Header from '../../components/Header'
 import HistoricoList from '../../components/HistoricoList'
@@ -14,27 +16,47 @@ import {
 
 
 export default function Home() {
-  const [ historico, setHistorico ] = useState([
-    {key: '1', tipo: 'receita', valor: 1200},
-    {key: '2', tipo: 'despesa', valor: 200},
-    {key: '3', tipo: 'receita', valor: 40},
-    {key: '4', tipo: 'receita', valor: 89.62},
-    {key: '5', tipo: 'despesa', valor: 501.00},
-    {key: '6', tipo: 'despesa', valor: 199.00},
-    {key: '7', tipo: 'receita', valor: 1299.00},
-  ])
+  const [ historico, setHistorico ] = useState([])
+  const [saldo, setSaldo] = useState(0)
   
   const { user } = useContext(AuthContext)
+  const uid = user && user.uid
+
+  useEffect(() => {
+    async function loadList() {
+      await firebase.database().ref('users').child(uid).on('value', (snapshot) =>{
+        setSaldo(snapshot.val().saldo)
+      })
+      await firebase.database().ref('historico')
+        .child(uid)
+        .orderByChild('date').equalTo(format(new Date, 'dd/MM/yy'))
+        .limitToLast(10).on('value', (snapshot) => {
+          setHistorico([])
+          snapshot.forEach((childItem) => {
+            let list = {
+              key: childItem.key,
+              tipo: childItem.val().tipo,
+              valor: childItem.val().valor
+            }
+
+            setHistorico(oldArray => [...oldArray, list].reverse())
+          })
+        })
+    }
+
+    loadList()
+  }, [])
 
   return (
     <Background>
       <Header />
       <Container>
         <Nome>{user && user.nome}</Nome>
-        <Saldo>R$ 145,00</Saldo>
+        <Saldo>R$ { saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</Saldo>
       </Container>
 
       <Title>Ultimas movimentações</Title>
+
       <List 
         showsVerticalScrollIndicator={false}
         data={historico}
