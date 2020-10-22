24d94487b8c7d8c85150700eeae09d72b31d4a15
leandross86 <leandrobko@gmail.com>
2020-10-21 22:33:15 -0300

    [CREDENTIALS] Change credentials firebase

--- src/services/firebaseConnetion.js
+++ src/services/firebaseConnetion.js
@@ -3,15 +3,15 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAebl316l6dnWe9daLSrZsV9rDHCDKNH-Y',
-  authDomain: 'bkofinancas.firebaseapp.com',
-  databaseURL: 'https://bkofinancas.firebaseio.com',
-  projectId: 'bkofinancas',
-  storageBucket: 'bkofinancas.appspot.com',
-  messagingSenderId: '970033569828',
-  appId: '1:970033569828:web:e1b0e3f1526d0ad436b8b1',
-  measurementId: 'G-JSPB93SZRM'
-}
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+  };
 // Initialize Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
